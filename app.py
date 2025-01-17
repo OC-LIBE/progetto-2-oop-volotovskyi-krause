ffb0cddb2d6dca844175b477e6dc9ca9bdcf7cfe
Yegor <yegorvolot@gmail.com>
2025-01-17 14:43:17 +0000

    Correzioni 17.01.2025

--- app.py
+++ app.py
@@ -2,42 +2,118 @@ import streamlit as st
 from modules.card import Card
 from modules.deck import Deck
 
+
 st.set_page_config(
    layout="wide",
 )
 card_width=95
 
 
-number_of_decks = st.number_input("Number of decks", min_value=1, max_value=10, value=1)
 
-deck = Deck(number_of_decks)
+deck = Deck(1)
+shuffled = False 
 
 
-st.markdown(f"## Deck created with {number_of_decks} deck/s")
+#st.markdown(f"## Deck created with {number_of_decks} deck/s")
 
-st.image([card.image for card in deck.cards], width=card_width)
+#st.image([card.image for card in deck.cards], width=card_width)
 
-st.markdown("## Shuffling deck")
-shuffle_button = st.button("Shuffle")
-if shuffle_button:
-    deck.shuffle()
-st.image([card.image for card in deck.cards], width=card_width)
+#st.markdown("## Shuffling deck")
+ 
+
+if st.button("Shuffle Deck"):
+    if shuffled == False:
+        deck.shuffle()
+        shuffled = True
+        st.success("Deck shuffled!")
+    else:
+        st.write("ok")
+
+st.write(shuffled)
+
+
+
 
 
 class Player:
-    def __init__(self, score):
-        self.score = score
-    
+    def __init__(self):
+        self.score = 0
+        self.handlist = []
 
     def hand(self):
-        self.hand1 = []
+        
+        st.image([card.image for card in deck.cards], width=card_width)
+        self.handlist = []
         length = 2
         for i in range(length):
-            self.hand1.append(deck.cards[i])
-        st.image([card.image for card in self.hand1], width=card_width)    
-        return self.hand1
+            self.handlist.append(deck.cards[i])
+        del deck.cards[:2]
+        #st.image([card.image for card in self.hand], width=card_width)
+        return self.handlist
+    
+    
+    def calc_score(self):
+        for i in self.handlist:
+            obj = i
+            a=(getattr(obj, 'rank'))
+            self.score = self.score + a
+        return self.score
+
+class Dealer:
+    def __init__(self):
+        self.score = 0
+        self.handlist = []
 
 
-if st.button("button"):
-    hand = Player(2).hand()
-    st.write(hand)
+
+
+player = Player()
+hand = player.hand()
+
+
+
+
+
+
+#if st.button("button"):
+    #hand = Player(2).hand()
+    #st.image([card.image for card in hand], width=card_width)
+    #st.write(hand)
+    
+   # for i in hand:
+       # obj = i
+        #st.write(getattr(obj, 'rank'))
+
+    #for i in hand:
+        
+#taking card
+if st.button("take card"):
+    player.handlist.append(deck.cards[0])
+    del deck.cards[:1]
+    st.write(player.handlist)
+    
+
+st.image([card.image for card in player.handlist], width=card_width)
+
+#score
+plscore = player.calc_score()
+st.markdown(f'Your score: {plscore} Dealer Score: ')
+if plscore > 21:
+    st.error('You lost')
+
+#columns
+col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns(11, gap="small")
+def col_insert(col_num):
+    try:
+        st.image(hand[col_num-1].image,width=card_width)
+    except TypeError:
+        print('')
+    except IndexError:
+        print('')
+
+with col1:
+    col_insert(1)
+with col2:
+    col_insert(2)
+with col3:
+    col_insert(3)
