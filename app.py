3e545d9bbf2ac5a08d95dccb7856d3373d8f41a5
Yegor <yegorvolot@gmail.com>
2025-01-27 19:33:06 +0000

    Correzioni 27.01.2025

--- app.py
+++ app.py
@@ -36,13 +36,14 @@ class Player:
     def __init__(self):
         self.score = 0
         self.handlist = []
-        self.hand()
         if 'handlist' not in st.session_state:
             st.session_state.handlist = self.handlist
+            self.hand_ap()
+            self.hand_ap()
         self.handf = st.session_state.handlist
     
 
-    def hand(self):
+    def hand_ap(self):
         drawn_card = deck.draw()
         self.handlist.append(drawn_card)
         return self.handlist
@@ -55,27 +56,34 @@ class Player:
             self.score = self.score + a
         return self.score
 
+player = Player()
 
 
 class HumanPlayer(Player):
     def __init__(self):
-        self.score = Player.score
+        self.name = "Player1"
 
 
-
-class Dealer:
+#class Dealer(Player):
     def __init__(self):
-        self.score = 0
+        self.name = 'Dealer'
         self.handlist = []
+        if 'handlist1' not in st.session_state:
+            st.session_state.handlist = self.handlist
+            self.hand_ap()
+            self.hand_ap()
+        self.hand1 = st.session_state.handlist
 
 
 
-player = Player()
+#dealer = Dealer()
+#st.write(dealer.hand1)
+#st.image([card.image for card in dealer.hand1], width=card_width)
 
 
-if st.button("take card"):
+if st.button("Hit"):
     player.handf.append(deck.cards[0])
-    #del deck.cards[0]
+    player.hand_ap()
     st.write(player.handf)
     
 
@@ -88,24 +96,24 @@ if plscore > 21:
     st.error('You lost')
 
 #columns
-col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns(11, gap="small")
-def col_insert(col_num):
-    try:
-        st.image(player.handf[col_num-1].image,width=card_width)
-    except TypeError:
-        print('')
-    except IndexError:
-        print('')
+#col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11 = st.columns(11, gap="small")
+#def col_insert(col_num):
+ #   try:
+  #      st.image(player.handf[col_num-1].image,width=card_width)
+ #   except TypeError:
+  #      print('')
+  #  except IndexError:
+  #      print('')
 
-with col1:
-    col_insert(1)
-with col2:
-    col_insert(2)
-with col3:
-    col_insert(3)
-with col4:
-    col_insert(4)
-with col5:
-    col_insert(5)
-with col6:
-    col_insert(6)
+#with col1:
+  #  col_insert(1)
+#with col2:
+   # col_insert(2)
+#with col3:
+  #  col_insert(3)
+#with col4:
+   # col_insert(4)
+#with col5:
+   # col_insert(5)
+#with col6:
+   # col_insert(6)
